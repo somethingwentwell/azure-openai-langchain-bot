89738d1a50e3b78a662980238586e502c13330e0
Warren <wwwc@outlook.com>
2023-05-08 13:25:54 +0800

    add zapier & db

---
+++ init.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE chat_history;
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from langchain.chat_models import AzureChatOpenAI
 from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
+from langchain.memory import PostgresChatMessageHistory
 from docsimport import allImportedTools
 from customtools import allCustomTools
 from dotenv import load_dotenv
 import os
-os.environ["LANGCHAIN_HANDLER"] = "langchain"
+# os.environ["LANGCHAIN_HANDLER"] = "langchain"
 
 load_dotenv()
 
@@ -24,11 +25,12 @@ azchat=AzureChatOpenAI(
 )
 
 memories = {}
+history = {}
 agents = {}
 agent_chains = {}
 
 tools = []
-tools.extend(allImportedTools(os.getenv("TOOLS_CATEGORY"), azllm))
+# tools.extend(allImportedTools(os.getenv("TOOLS_CATEGORY"), azllm))
 tools.extend(allCustomTools())
 
 
@@ -37,9 +39,9 @@ tool_names = [tool.name for tool in tools]
 def SetupChatAgent(id):
     # memories[id] = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     memories[id] = ConversationSummaryBufferMemory(llm=azchat, max_token_limit=1000, memory_key="chat_history", return_messages=True)
-    memories[id].save_context({"input": os.getenv("CHAT_SYSTEM_PROMPT")}, {"ouputs": "Copy that"})
-    
-    agent_chains[id] = initialize_agent(tools, azchat, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memories[id], max_iterations=2, early_stopping_method="generate")
+    memories[id].save_context({"input": os.getenv("CHAT_SYSTEM_PROMPT")}, {"ouputs": "I will try my best to help for the upcoming questions."})
+    history[id] = PostgresChatMessageHistory(connection_string="postgresql://postgres:postgres@host.docker.internal:5432/chat_history", session_id=str(id))
+    agent_chains[id] = initialize_agent(tools, azchat, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memories[id], max_iterations=5, early_stopping_method="generate")
 
 # loop tools descriptions
 print("-----TOOLS-----")
@@ -71,14 +73,16 @@ def keepAsking(mid, text):
 def clearMemory(mid):
     newMessage = os.getenv("CHAT_SYSTEM_PROMPT") + "\nThe summary as below:\n" + agent_chains[mid].memory.predict_new_summary(agent_chains[mid].memory.buffer, agent_chains[mid].memory.moving_summary_buffer)
     agent_chains[mid].memory.buffer.clear()
-    agent_chains[mid].memory.save_context({"input": newMessage}, {"ouputs": "Copy that"})
+    agent_chains[mid].memory.save_context({"input": newMessage}, {"ouputs": "I will try my best to help for the upcoming questions."})
 
 @app.post("/run")
 def run(msg: MessageReq):
     if (msg.id not in agent_chains):
         SetupChatAgent(msg.id)
-    # response = agent_chains[msg.id].run(input=msg.text)
-    response = keepAsking(msg.id, msg.text)
+    response = agent_chains[msg.id].run(input=msg.text)
+    history[msg.id].add_user_message(msg.text)
+    history[msg.id].add_ai_message(response)
+    # response = keepAsking(msg.id, msg.text)
     # clearMemory(msg.id)
     print("------MEMORY ID: " + msg.id + "-----")
     if (agent_chains[msg.id].memory.llm.get_num_tokens_from_messages(agent_chains[msg.id].memory.buffer) > 500):
